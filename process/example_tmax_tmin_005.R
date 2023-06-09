b8fe92429363c7f28083dfce20f66be9fa3bfd6a
adrhuerta <adrhuerta@gmail.com>
2023-06-09 00:34:14 -0500

    merging examples

---
+++ process/example_tmax_tmin_005.R
@@ -0,0 +1,27 @@
+rm(list = ls())
+
+library(raster)
+library(gstat)
+"%>%" = magrittr::`%>%`
+
+source('./src/005/Merging/MG_make_covariables.R')
+source('./src/005/Merging/MG_RK.R')
+
+# gridded
+LST_day <- raster::brick("data/covariables/005/LST_DAY.nc")
+LST_night <- raster::brick("data/covariables/005/LST_NIGHT.nc")
+DEM <- raster::raster("data/covariables/005/DEM.nc")
+X <- raster::raster("data/covariables/005/X.nc")
+Y <- raster::raster("data/covariables/005/Y.nc")
+tdi <- raster::raster("data/covariables/005/TDI.nc")
+
+# gridded
+tmax_normals <- raster::brick("data/climatologies/tmax_mean_1981-2010_005.nc") 
+tmin_normals <- raster::brick("data/climatologies/tmin_mean_1981-2010_005.nc")
+
+# making list of covs
+covs_list_tmax <- list(dynamic = list(LST = LST_day),
+                       static = list(DEM = DEM, X = X, Y = Y, TDI = tdi))
+
+covs_list_tmin <- list(dynamic = list(LST = LST_night),
+                       static = list(DEM = DEM, X = X, Y = Y, TDI = tdi))
